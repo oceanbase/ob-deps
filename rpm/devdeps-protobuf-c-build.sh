34dd1f2c26a465794c3fa5926554a62814b4c2dc
淮新 <huaixin.lmy@digital-engine.com>
2025-03-27 10:10:34 +0800

    Upgrade protobufc to 1.5.1

--- rpm/devdeps-protobuf-c-build.sh
+++ rpm/devdeps-protobuf-c-build.sh
@@ -4,8 +4,11 @@ CUR_DIR=$(dirname $(readlink -f "$0"))
 ROOT_DIR=$CUR_DIR/..
 PROJECT_DIR=${1:-"$ROOT_DIR"}
 PROJECT_NAME=${2:-"devdeps-protobuf-c"}
-VERSION=${3:-"1.4.1"}
+VERSION=${3:-"1.5.1"}
 RELEASE=${4:-"2"}
+
+# Configure custom source file directory
+[ -n "$SOURCE_DIR" ] && mv $SOURCE_DIR/* $ROOT_DIR
 
 # check source code
 if [[ -z `find $ROOT_DIR -maxdepth 1 -regex ".*/protobuf-all-3.20.3.*[tar|gz|bz2|xz|zip]$"` ]]; then
@@ -15,8 +18,41 @@ fi
 
 if [[ -z `find $ROOT_DIR -maxdepth 1 -regex ".*/protobuf-c-$VERSION.*[tar|gz|bz2|xz|zip]$"` ]]; then
     echo "Download source code"
-    wget https://github.com/protobuf-c/protobuf-c/releases/download/v${VERSION}/protobuf-c-${VERSION}.tar.gz -O $ROOT_DIR/protobuf-c-$VERSION.tar.gz
+    wget https://github.com/protobuf-c/protobuf-c/archive/refs/tags/v${VERSION}.tar.gz -O $ROOT_DIR/protobuf-c-$VERSION.tar.gz --no-check-certificate
 fi
+
+ID=$(grep -Po '(?<=^ID=).*' /etc/os-release | tr -d '"')
+ 
+if [[ "${ID}"x == "alinux"x ]]; then
+    wget http://mirrors.aliyun.com/oceanbase/OceanBaseAlinux.repo -P /etc/yum.repos.d/
+    yum install -y obdevtools-gcc9-9.3.0
+else
+    os_release=`grep -Po '(?<=release )\d' /etc/redhat-release`
+    arch=`uname -p`
+    dep_pkgs=(obdevtools-gcc9-9.3.0-52022092914.el)
+ 
+    target_dir_3rd=${PROJECT_DIR}/deps/3rd
+    pkg_dir=$target_dir_3rd/pkg
+    mkdir -p $pkg_dir
+    for dep_pkg in ${dep_pkgs[@]}
+    do
+        TEMP=$(mktemp -p "/" -u ".XXXX")
+        download_base_url="https://mirrors.aliyun.com/oceanbase/development-kit/el"
+        deps_url=${download_base_url}/${os_release}/${arch}
+        pkg=${dep_pkg}${os_release}.${arch}.rpm
+        wget $deps_url/$pkg -O $pkg_dir/$TEMP
+        if [[ $? == 0 ]]; then
+            mv -f $pkg_dir/$TEMP $pkg_dir/$pkg
+        fi
+        (cd / && rpm2cpio $pkg_dir/$pkg | cpio -di -u --quiet)
+    done
+fi
+
+export TOOLS_DIR=/usr/local/oceanbase/devtools
+export PATH=$TOOLS_DIR/bin:$PATH
+export LD_LIBRARY_PATH=$TOOLS_DIR/lib:$TOOLS_DIR/lib64:$LD_LIBRARY_PATH
+export CC=$TOOLS_DIR/bin/gcc
+export CXX=$TOOLS_DIR/bin/g++
 
 cd $CUR_DIR
 bash $CUR_DIR/rpmbuild.sh $PROJECT_DIR $PROJECT_NAME $VERSION $RELEASE
--- rpm/devdeps-protobuf-c.spec
+++ rpm/devdeps-protobuf-c.spec
@@ -1,11 +1,11 @@
 Name: devdeps-protobuf-c
-Version: 1.4.1
+Version: 1.5.1
 Release: %(echo $RELEASE)%{?dist}
 
 Summary: This is protobuf-c, a C implementation of the Google Protocol Buffers data serialization format. 
 
 License:  BSD-2-Clause
-Url: https://github.com/protobuf-c/protobuf-c/tree/v1.4.1
+Url: https://github.com/protobuf-c/protobuf-c/
 AutoReqProv:no
 
 %undefine _missing_build_ids_terminate_build
@@ -14,7 +14,7 @@ AutoReqProv:no
 %define _buliddir %{_topdir}/BUILD
 %define _tmppath %{_buliddir}/_tmp
 %define _prefix /usr/local/oceanbase/deps/devel
-%define _protobufc protobuf-c-1.4.1
+%define _protobufc protobuf-c-%{version}
 %define _proto protobuf-all-3.20.3
 %define _proto_dir protobuf-3.20.3
 %define debug_package %{nil}
@@ -25,23 +25,32 @@ protobuf-c is a C implementation of the Google Protocol Buffers data serializati
 %install
 mkdir -p %{buildroot}/%{_prefix}/lib/protobuf-c
 mkdir -p %{buildroot}/%{_prefix}/include/protobuf-c
+CPU_CORES=`grep -c ^processor /proc/cpuinfo`
+sed -i "s|^\(libdir=\).*$|\1'${TOOLS_DIR}/lib64/'|" ${TOOLS_DIR}/lib64/libstdc++.la
+export CPPFLAGS="-D_GLIBCXX_USE_CXX11_ABI=0"
+
+# build protobuf-all
 cd $OLDPWD/../
 rm -rf %{_proto_dir}
 tar xf %{_proto}.tar.gz
 cd %{_proto_dir}
 ./configure --prefix=%{_tmppath}/proto
-CPU_CORES=`grep -c ^processor /proc/cpuinfo`
 make -j${CPU_CORES}
 make install
 
+# build protobuf-c
 cd %{_topdir}/../../
 rm -rf %{_protobufc}
 tar xf %{_protobufc}.tar.gz
 cd %{_protobufc}
 export PKG_CONFIG_PATH=%{_tmppath}/proto/lib/pkgconfig
-./configure --prefix=%{_tmppath} --enable-shared=yes CFLAGS="-g -O2 -fPIC" CXXFLAGS="-g -O2 -fPIC"
+export LDFLAGS="-pie -z noexecstack -z now"
+export CFLAGS="-fPIC -g -O2 -pie -fstack-protector-strong"
+export CXXFLAGS="-fPIC -g -O2 -pie -fstack-protector-strong"
+./autogen.sh && ./configure --prefix=%{_tmppath} --enable-shared=yes
 make -j${CPU_CORES}
 make install
+
 cp %{_tmppath}/include/protobuf-c/*.h %{buildroot}/%{_prefix}/include/protobuf-c
 cp %{_tmppath}/lib/*.a %{buildroot}/%{_prefix}/lib/
 
