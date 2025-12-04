d4b6f8fbf29a834148f4f67c2b21d2fa5c34639e
huaixin.lmy <huaixin.lmy@oceanbase.com>
2025-12-04 16:49:30 +0800

    update devdeps-openssl-build.sh

--- rpm/devdeps-openssl-build.sh
+++ rpm/devdeps-openssl-build.sh
@@ -10,10 +10,4 @@ RELEASE=${4:-"1"}
 # Configure custom source file directory
 [ -n "$SOURCE_DIR" ] && mv $SOURCE_DIR/* $ROOT_DIR
 
-# check source code
-if [[ -z `find $ROOT_DIR -maxdepth 1 -regex ".*/openssl-$VERSION.*[tar|gz|bz2|xz|zip]$"` ]]; then
-    echo "Download ${PROJECT_NAME} source code"
-    wget --no-check-certificate https://github.com/openssl/openssl/releases/download/OpenSSL_1_1_1u/openssl-$VERSION.tar.gz -O $ROOT_DIR/openssl-$VERSION.tar.gz --no-check-certificate
-fi
-
 bash $CUR_DIR/$PROJECT_NAME-$VERSION.sh $PROJECT_DIR $PROJECT_NAME $VERSION $RELEASE
