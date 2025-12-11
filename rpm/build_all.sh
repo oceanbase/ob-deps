38008c2a88a845ca1dbb6fc02d7e47c7d455b196
huaixin.lmy <huaixin.lmy@oceanbase.com>
2025-12-11 18:04:57 +0800

    add llvm

--- rpm/build_all.sh
+++ rpm/build_all.sh
@@ -11,7 +11,7 @@ echo "=============================="
 
 # 所有要构建的项目脚本名（不带 .sh）
 projects=(
-  "devdeps-openssl-build.sh"
+  "obdevtools-llvm-17.0.6.sh"
   "devdeps-abseil-cpp-build.sh"
   "devdeps-apache-arrow-build.sh"
   "devdeps-apache-orc-build.sh"
@@ -23,6 +23,7 @@ projects=(
   "devdeps-lua-build.sh"
   "devdeps-mariadb-connector-c-build.sh"
   "devdeps-mxml-build.sh"
+  "devdeps-openssl-build.sh"
   "devdeps-protobuf-c-build.sh"
   "devdeps-relaxed-rapidjson-build.sh"
   "devdeps-roaringbitmap-croaring-build.sh"
---
+++ rpm/obdevtools-llvm-17.0.6.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+CUR_DIR=$(dirname $(readlink -f "$0"))
+ROOT_DIR=$CUR_DIR/..
+PROJECT_DIR=${1:-"$ROOT_DIR"}
+PROJECT_NAME=${2:-"obdevtools-llvm"}
+VERSION=${3:-"17.0.6"}
+RELEASE=${4:-"1"}
+
+# check source code
+if [[ -z `find $ROOT_DIR -maxdepth 1 -regex ".*/clang+llvm-$VERSION.*[tar|gz|bz2|xz|zip]$"` ]]; then
+    echo "Download ${PROJECT_NAME} source code"
+    wget --no-check-certificate https://github.com/llvm/llvm-project/releases/download/llvmorg-17.0.6/clang+llvm-17.0.6-arm64-apple-darwin22.0.tar.xz -O $ROOT_DIR/clang+llvm-$VERSION.tar.xz
+fi
+
+# init build package env
+echo "[BUILD] args: CURDIR=${CUR_DIR} PROJECT_NAME=${PROJECT_NAME} VERSION=${VERSION} RELEASE=${RELEASE}"
+CPU_CORES=$(sysctl -n hw.ncpu 2>/dev/null || grep -c ^processor /proc/cpuinfo)
+TOP_DIR=$CUR_DIR/.pkg_build/usr/local/oceanbase/deps/devel
+rm -rf $TOP_DIR && mkdir -p $TOP_DIR
+
+# compile and install
+TMP_DIR=$CUR_DIR/$PROJECT_NAME
+rm -rf $TMP_DIR && mkdir -p $TMP_DIR
+TMP_INSTALL=$TMP_DIR/tmp_install
+rm -rf $TMP_INSTALL && mkdir -p $TMP_INSTALL
+cd $TMP_DIR
+tar -xf $ROOT_DIR/clang+llvm-$VERSION.tar.xz
+cd clang+llvm-17.0.6-arm64-apple-darwin22.0
+
+# copy install file
+cp -r * $TOP_DIR/
+cd $TOP_DIR/bin
+ln -sf clang-17 clang++-17
+
+# build package
+echo "[BUILD] build tarball......"
+cd $CUR_DIR/.pkg_build/
+tar -zcvf ${PROJECT_NAME}-${VERSION}-${RELEASE}.tar.gz ./usr
+mv ${PROJECT_NAME}-${VERSION}-${RELEASE}.tar.gz ${CUR_DIR}
