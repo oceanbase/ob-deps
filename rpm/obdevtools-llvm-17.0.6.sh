d5690311898b74eea71d0bb1373b8352dae57709
huaixin.lmy <huaixin.lmy@oceanbase.com>
2025-12-12 17:59:35 +0800

    update llvm 17

--- rpm/obdevtools-llvm-17.0.6.sh
+++ rpm/obdevtools-llvm-17.0.6.sh
@@ -8,15 +8,23 @@ VERSION=${3:-"17.0.6"}
 RELEASE=${4:-"1"}
 
 # check source code
-if [[ -z `find $ROOT_DIR -maxdepth 1 -regex ".*/clang+llvm-$VERSION.*[tar|gz|bz2|xz|zip]$"` ]]; then
+if [[ -z `find $ROOT_DIR -maxdepth 1 -regex ".*/llvm-${VERSION}.*[tar|gz|bz2|xz|zip]$"` ]]; then
     echo "Download ${PROJECT_NAME} source code"
-    wget --no-check-certificate https://github.com/llvm/llvm-project/releases/download/llvmorg-17.0.6/clang+llvm-17.0.6-arm64-apple-darwin22.0.tar.xz -O $ROOT_DIR/clang+llvm-$VERSION.tar.xz
+    wget https://github.com/llvm/llvm-project/releases/download/llvmorg-${VERSION}/llvm-${VERSION}.src.tar.xz -P $ROOT_DIR --no-check-certificate
+    wget https://github.com/llvm/llvm-project/releases/download/llvmorg-${VERSION}/lld-${VERSION}.src.tar.xz -P $ROOT_DIR --no-check-certificate
+    wget https://github.com/llvm/llvm-project/releases/download/llvmorg-${VERSION}/lldb-${VERSION}.src.tar.xz -P $ROOT_DIR --no-check-certificate
+    wget https://github.com/llvm/llvm-project/releases/download/llvmorg-${VERSION}/clang-${VERSION}.src.tar.xz -P $ROOT_DIR --no-check-certificate
+    wget https://github.com/llvm/llvm-project/releases/download/llvmorg-${VERSION}/compiler-rt-${VERSION}.src.tar.xz -P $ROOT_DIR --no-check-certificate
+    wget https://github.com/llvm/llvm-project/releases/download/llvmorg-${VERSION}/libunwind-${VERSION}.src.tar.xz -P $ROOT_DIR --no-check-certificate
+    wget https://github.com/llvm/llvm-project/releases/download/llvmorg-${VERSION}/third-party-${VERSION}.src.tar.xz -P $ROOT_DIR --no-check-certificate
+    wget https://github.com/llvm/llvm-project/releases/download/llvmorg-${VERSION}/cmake-${VERSION}.src.tar.xz -P $ROOT_DIR --no-check-certificate
 fi
 
 # init build package env
 echo "[BUILD] args: CURDIR=${CUR_DIR} PROJECT_NAME=${PROJECT_NAME} VERSION=${VERSION} RELEASE=${RELEASE}"
 CPU_CORES=$(sysctl -n hw.ncpu 2>/dev/null || grep -c ^processor /proc/cpuinfo)
-TOP_DIR=$CUR_DIR/.pkg_build/usr/local/oceanbase/deps/devel
+rm -rf $CUR_DIR/.pkg_build
+TOP_DIR=$CUR_DIR/.pkg_build/usr/local/oceanbase/devtools
 rm -rf $TOP_DIR && mkdir -p $TOP_DIR
 
 # compile and install
@@ -25,11 +33,54 @@ rm -rf $TMP_DIR && mkdir -p $TMP_DIR
 TMP_INSTALL=$TMP_DIR/tmp_install
 rm -rf $TMP_INSTALL && mkdir -p $TMP_INSTALL
 cd $TMP_DIR
-tar -xf $ROOT_DIR/clang+llvm-$VERSION.tar.xz
-cd clang+llvm-17.0.6-arm64-apple-darwin22.0
+
+# prep
+mkdir -p llvm_src_dir && cd llvm_src_dir
+tar -xf $ROOT_DIR/clang-${VERSION}.src.tar.xz
+tar -xf $ROOT_DIR/cmake-${VERSION}.src.tar.xz
+tar -xf $ROOT_DIR/compiler-rt-${VERSION}.src.tar.xz
+tar -xf $ROOT_DIR/libunwind-${VERSION}.src.tar.xz
+tar -xf $ROOT_DIR/lld-${VERSION}.src.tar.xz
+tar -xf $ROOT_DIR/lldb-${VERSION}.src.tar.xz
+tar -xf $ROOT_DIR/llvm-${VERSION}.src.tar.xz
+tar -xf $ROOT_DIR/third-party-${VERSION}.src.tar.xz
+
+mv clang-${VERSION}.src clang
+mv cmake-${VERSION}.src cmake
+mv compiler-rt-${VERSION}.src compiler-rt
+mv libunwind-${VERSION}.src libunwind
+mv lld-${VERSION}.src lld
+mv lldb-${VERSION}.src lldb
+mv llvm-${VERSION}.src llvm
+mv third-party-${VERSION}.src third-party
+
+cp ${ROOT_DIR}/patch/fix-outline-atomics.patch ./
+patch -p1 < fix-outline-atomics.patch
+
+# build Release or RelWithDebInfo version
+rm -rf build-rpm && mkdir -p build-rpm
+cd build-rpm
+
+cmake ../llvm  \
+    -DCMAKE_INSTALL_PREFIX=${TMP_INSTALL} \
+    -DLLVM_TARGETS_TO_BUILD="AArch64;X86" \
+    -DLLVM_ENABLE_RTTI=ON \
+    -DLLVM_ENABLE_EH=ON \
+    -DLLVM_ENABLE_DUMP=ON \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DLLVM_ENABLE_PROJECTS='clang;compiler-rt;lld' \
+    -DCOMPILER_RT_EXCLUDE_ATOMIC_BUILTIN=ON \
+    -DDARWIN_osx_SKIP_CC_KEXT=ON \
+    -DCOMPILER_RT_ENABLE_IOS=OFF \
+    -DCOMPILER_RT_ENABLE_WATCHOS=OFF \
+    -DCOMPILER_RT_ENABLE_TVOS=OFF \
+    -G 'Unix Makefiles'
+
+make -j${CPU_CORES}
+make install
 
 # copy install file
-cp -r * $TOP_DIR/
+cp -r ${TMP_INSTALL}/* $TOP_DIR/
 cd $TOP_DIR/bin
 ln -sf clang-17 clang++-17
 
