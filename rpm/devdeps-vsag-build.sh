435aa27b25a38fde61787bd59a3956120a93efc3
ningxin.ning <ningxin.ning@oceanbase.com>
2026-01-12 13:09:16 +0800

    upgrade vsag to 0.16.16

--- rpm/devdeps-vsag-build.sh
+++ rpm/devdeps-vsag-build.sh
@@ -10,7 +10,7 @@ if [[ $VERSION == "1.0.0" ]]; then
   VSAG_VERSION="0.14.7"
 else
   # default use newest vsag
-  VSAG_VERSION="0.16.10"
+  VSAG_VERSION="0.16.16"
 fi
 
 echo "VERSION=${VERSION} VSAG_VERSION=${VSAG_VERSION}"
