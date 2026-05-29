9ab5a6c4d4a97cea80083712e5c2925106b807fe
huaixin.lmy <huaixin.lmy@digital-engine.com>
2026-05-29 16:14:56 +0800

    upgrade 5.4.8

--- rpm/devdeps-lua-build.sh
+++ rpm/devdeps-lua-build.sh
@@ -2,13 +2,13 @@ CUR_DIR=$(dirname $(readlink -f "$0"))
 ROOT_DIR=$CUR_DIR/../
 PROJECT_DIR=${1:-"$CUR_DIR"}
 PROJECT_NAME=${2:-"devdeps-lua"}
-VERSION=${3:-"5.4.3"}
+VERSION=${3:-"5.4.8"}
 RELEASE=${4:-"9"}
 
 # check source code
 if [[ -z `find $ROOT_DIR -maxdepth 1 -regex ".*/lua-$VERSION.*[tar|gz|bz2|xz|zip]$"` ]]; then
     echo "Download source code"
-    wget http://www.lua.org/ftp/lua-5.4.3.tar.gz -O $ROOT_DIR/lua-$VERSION.tar.gz --no-check-certificate
+    wget http://www.lua.org/ftp/lua-$VERSION.tar.gz -O $ROOT_DIR/lua-$VERSION.tar.gz --no-check-certificate
 fi
 
 cd $CUR_DIR
