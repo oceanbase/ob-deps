bb1ba696fe8ad544522bb44cc9e985abd5472d65
huaixin.lmy <huaixin.lmy@digital-engine.com>
2026-06-25 14:45:08 +0800

    support drcmsg

--- rpm/devdeps-drcmsg-build.sh
+++ rpm/devdeps-drcmsg-build.sh
@@ -4,6 +4,7 @@ CUR_DIR=$(dirname $(readlink -f "$0"))
 ROOT_DIR=$CUR_DIR/..
 PROJECT_NAME=${2:-"devdeps-drcmsg"}
 VERSION=${3:-"1.1"}
+TOP_DIR=$CUR_DIR/.rpm_build
 
 # check source code
 if [[ ! -d "$ROOT_DIR/drcmsg" ]]; then
@@ -16,9 +17,13 @@ if [[ ! -d "$ROOT_DIR/drcmsg/deps/3rd/drcmsg.an8.loongarch64.deps" ]]; then
     cd $ROOT_DIR/drcmsg
     git apply $ROOT_DIR/loongarch/drcmsg.diff
     cp $ROOT_DIR/loongarch/drcmsg.an8.loongarch64.deps deps/3rd
-    cd $CUR_DIR
 fi
-SPEC_DIR=$ROOT_DIR/drcmsg/rpm
 
-rpmbuild --define "_topdir $SPEC_DIR" -bb $PROJECT_NAME.spec
-find $TOP_DIR/ -name "*.rpm" -exec mv {} . 2>/dev/null \;
+cd $ROOT_DIR/drcmsg/rpm
+
+# prepare rpm build dirs
+rm -rf $TOP_DIR
+mkdir -p $TOP_DIR/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+
+rpmbuild --define "_topdir $TOP_DIR" -bb $PROJECT_NAME.spec
+find $TOP_DIR/ -name "*.rpm" -exec mv {} "$CUR_DIR" 2>/dev/null \;
